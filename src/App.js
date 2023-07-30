6daf7e5709c3a863e8318e793fa51a316179384b
Anton <Anton.Suleimanov@gmail.com>
2023-07-30 20:19:46 +0300

    1:15:17 searchQuery - done

--- src/App.js
+++ src/App.js
@@ -8,51 +8,66 @@ import MySelect from "./components/UI/select/MySelect";
 
 function App() {
   
-  const [posts, setPosts] = useState([
-    {id: 1, title: 'аа', body: 'бб'},
-    {id: 2, title: 'гг 2', body: 'аа'},
-    {id: 3, title: 'вв 3', body: 'яя'}
-  ])
+    const [posts, setPosts] = useState([
+      {id: 1, title: 'аа', body: 'бб'},
+      {id: 2, title: 'гг 2', body: 'аа'},
+      {id: 3, title: 'вв 3', body: 'яя'}
+    ])
 
-  const [selectedSort, setSelectedSort] = useState('')
+    const [selectedSort, setSelectedSort] = useState('')
+    const [searchQuery, setSearchQuery] = useState('')
 
-  const createPost = (newPost) => {
-      setPosts([...posts, newPost])
-  }
+    function getSortedPosts() {
+        console.log('sortedPosts works fine')
+        if (selectedSort) {
+            return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+        }
+        return posts;
+    }
 
-  const removePost = (post) => {
-      setPosts(posts.filter(p => p.id !== post.id))
-  }
+    const sortedPosts = getSortedPosts()
 
-  const sortPosts = (sort) => {
-      setSelectedSort(sort);
-      setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
-  }
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
+    }
 
-  return (
-    <div className="App">
-      <PostForm create={createPost} />
-      <hr style={{margin: '15px 0'}} />
-      <div>
-          <MySelect
-            value={selectedSort}
-            onChange={sortPosts}
-            defaultValue="Sort by"
-            options={[
-                {value: 'title', name: 'By name'},
-                {value: 'body', name: 'By description'}
-            ]}
-          />
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
+    }
+
+    const sortPosts = (sort) => {
+        setSelectedSort(sort);
+    }
+
+    return (
+      <div className="App">
+        <PostForm create={createPost} />
+        <hr style={{margin: '15px 0'}} />
+        <div>
+            <MyInput
+                value={searchQuery}
+                onChange={e => setSearchQuery(e.target.value)}
+                placeholder="Search..."
+            />
+            <MySelect
+              value={selectedSort}
+              onChange={sortPosts}
+              defaultValue="Sort by"
+              options={[
+                  {value: 'title', name: 'By name'},
+                  {value: 'body', name: 'By description'}
+              ]}
+            />
+        </div>
+        {posts.length
+            ? <PostList remove={removePost} posts={sortedPosts} title='Posts about JS' style={{color: "teal"}} />
+            :
+            <h1 style={{textAlign: "center", color: "teal"}}>
+                Posts not found
+            </h1>
+        }
       </div>
-      {posts.length
-          ? <PostList remove={removePost} posts={posts} title='Posts about JS' />
-          :
-          <h1 style={{textAlign: "center", color: "teal"}}>
-              Posts not found
-          </h1>
-      }
-    </div>
-  );
+    );
 }
 
 export default App;
