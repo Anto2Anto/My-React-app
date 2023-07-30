77061c182b25c0a1f22dde6dc5de8a999631e5e9
Anton <Anton.Suleimanov@gmail.com>
2023-07-30 23:16:03 +0300

    1:23:01 - memoization/caching - done!

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/PostForm";
 import MySelect from "./components/UI/select/MySelect";
+import PostFilter from "./components/PostFilter";
 
 function App() {
   
@@ -14,16 +15,19 @@ function App() {
       {id: 3, title: 'вв 3', body: 'яя'}
     ])
 
-    const [selectedSort, setSelectedSort] = useState('')
-    const [searchQuery, setSearchQuery] = useState('')
+    const [filter, setFilter] = useState({sort: '', query: ''})
 
     const sortedPosts = useMemo(() => {
         console.log('sortedPosts works fine')
-        if (selectedSort) {
-            return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+        if (filter.sort) {
+            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
         }
         return posts;
-    }, [selectedSort, posts])
+    }, [filter.sort, posts])
+
+    const sortedAndSearchedPosts = useMemo(() => {
+        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
+    }, [filter.query, sortedPosts])
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
@@ -33,32 +37,16 @@ function App() {
         setPosts(posts.filter(p => p.id !== post.id))
     }
 
-    const sortPosts = (sort) => {
-        setSelectedSort(sort);
-    }
-
     return (
       <div className="App">
         <PostForm create={createPost} />
         <hr style={{margin: '15px 0'}} />
-        <div>
-            <MyInput
-                value={searchQuery}
-                onChange={e => setSearchQuery(e.target.value)}
-                placeholder="Search..."
-            />
-            <MySelect
-              value={selectedSort}
-              onChange={sortPosts}
-              defaultValue="Sort by"
-              options={[
-                  {value: 'title', name: 'By name'},
-                  {value: 'body', name: 'By description'}
-              ]}
-            />
-        </div>
-        {posts.length
-            ? <PostList remove={removePost} posts={sortedPosts} title='Posts about JS' style={{color: "teal"}} />
+        <PostFilter
+            filter={filter}
+            setFilter={setFilter}
+        />
+        {sortedAndSearchedPosts.length
+            ? <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Posts about JS' style={{color: "teal"}} />
             :
             <h1 style={{textAlign: "center", color: "teal"}}>
                 Posts not found
