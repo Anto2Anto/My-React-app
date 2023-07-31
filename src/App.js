22ffd696194c21896029993cdb9ce6a769a5157b
Anton <Anton.Suleimanov@gmail.com>
2023-07-31 20:39:42 +0300

    1:30:26 modal window  - done!

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/PostForm";
 import MySelect from "./components/UI/select/MySelect";
 import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/MyModal/MyModal";
 
 function App() {
   
@@ -16,6 +17,8 @@ function App() {
     ])
 
     const [filter, setFilter] = useState({sort: '', query: ''})
+
+    const [modal, setModal] = useState(false);
 
     const sortedPosts = useMemo(() => {
         console.log('sortedPosts works fine')
@@ -31,6 +34,7 @@ function App() {
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
+        setModal(false)
     }
 
     const removePost = (post) => {
@@ -39,12 +43,17 @@ function App() {
 
     return (
       <div className="App">
-        <PostForm create={createPost} />
-        <hr style={{margin: '15px 0'}} />
-        <PostFilter
-            filter={filter}
-            setFilter={setFilter}
-        />
+          <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
+              Create a post?
+          </MyButton>
+          <MyModal visible={modal} setVisible={setModal}>
+              <PostForm create={createPost} />
+          </MyModal>
+          <hr style={{margin: '15px 0'}} />
+          <PostFilter
+              filter={filter}
+              setFilter={setFilter}
+          />
         {sortedAndSearchedPosts.length
             ? <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Posts about JS' style={{color: "teal"}} />
             :
