6fc1c3e2a3f30341c712ff7d599a9f98fe8a8348
Anton <Anton.Suleimanov@gmail.com>
2023-08-06 15:04:58 +0300

    login + useContext - done!

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import MyInput from "../components/UI/input/MyInput";
 import MyButton from "../components/UI/button/MyButton";
+import {AuthContext} from "../context";
 
 const Login = () => {
+    const {isAuth, setIsAuth} = useContext(AuthContext);
+    const login = event => {
+        event.preventDefault();
+        setIsAuth(true);
+        localStorage.setItem('auth', 'true')
+    }
+
     return (
         <div>
             <h1>Page for login</h1>
-            <form>
+            <form onSubmit={login}>
                 <MyInput type='text' placeholder='Enter login' />
-                <MyInput type='password' placeholder='Enter password' />
+                <MyInput type='current-password' placeholder='Enter password' />
                 <MyButton>Enter</MyButton>
             </form>
         </div>
